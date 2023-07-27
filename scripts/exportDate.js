ca1a8376dc1761fb4a6cf0af8fb8122827b22de7
여다희 <yeodahui.dev@gmail.com>
2023-07-27 16:58:26 +0900

    수정: 로그 코드 삭제 및 주석 변경

--- scripts/exportDate.js
+++ scripts/exportDate.js
@@ -1,7 +1,6 @@
 const $date = document.querySelector("#date");
-console.log($date.textContent);
 
-// functions
+// 어제 날짜에 대한 정보를 담은 객체를 리턴
 const getYesterday = () => {
   const DAYS = ["일", "월", "화", "수", "목", "금", "토"];
 
