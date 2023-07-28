a6dac0e35f4b150e376031bca7a566dac75cd950
Kim-Sarang-S2 <sarang4264@naver.com>
2023-07-29 00:52:58 +0900

    수정: settings.html 글귀 변경

--- scripts/dark.js
+++ scripts/dark.js
@@ -4,17 +4,17 @@ const largerTextButton = document.querySelector("#larger-text input");
 const changeModeButton = document.querySelector("#setting-mode input");
 
 largerTextButton.addEventListener("click", () => {
-    if (wrap.classList.contains("enlarged")) {
-        wrap.classList.remove("enlarged");
+  if (wrap.classList.contains("enlarged")) {
+    wrap.classList.remove("enlarged");
     } else {
         wrap.classList.add("enlarged");
     }
-});
+  });
 
 changeModeButton.addEventListener("click", () => {
-        if (document.querySelector('body').classList.contains('dark-mode')) {
-            document.body.classList.remove("dark-mode");
-        } else {
-            document.body.classList.add("dark-mode");
-        }
-    });
+  if (document.querySelector('body').classList.contains('dark-mode')) {
+    document.body.classList.remove("dark-mode");
+    } else {
+        document.body.classList.add("dark-mode");
+    }
+  });
