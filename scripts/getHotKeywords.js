7a68ea28caf994c64668ee1fe9c7755ccd9f64b0
여다희 <yeodahui.dev@gmail.com>
2023-07-28 22:42:41 +0900

    추가: 가장 주목받은 키워드 불러오는 모듈 작성

---
+++ scripts/getHotKeywords.js
@@ -0,0 +1,33 @@
+const $keywordsList = document.querySelector("#keywords");
+
+const appendListItem = (target, number, content) => {
+  // <a href="url">number. content</a>
+  const a = document.createElement("a");
+  a.href = `/pages/search.html?q=${content}`;
+  a.textContent = `${number}. ${content}`;
+
+  // <li><a/></li>
+  const li = document.createElement("li");
+  li.appendChild(a);
+
+  target.appendChild(li);
+};
+
+const getHotKeywords = async () => {
+  try {
+    const file = "/scripts/data/top-liked.json";
+    const response = await axios.get(file);
+    const keywords = response.data.keywords;
+    console.log(keywords);
+
+    // #keywords 노드의 자식 노드 전부 제거 후 새로운 자식 노드를 추가
+    $keywordsList.replaceChildren();
+    keywords.map((item, index) => {
+      appendListItem($keywordsList, index + 1, item);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHotKeywords();
