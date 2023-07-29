8b108e98d0174a17d61a7449683f6f6f1439074e
Kim-Sarang-S2 <sarang4264@naver.com>
2023-07-29 20:52:01 +0900

    추가: 바 클릭시 토글버튼 활성화 - 1

--- scripts/dark.js
+++ scripts/dark.js
@@ -3,14 +3,32 @@ const wrap = document.querySelector(".wrap");
 const largerTextButton = document.querySelector("#larger-text input");
 const changeModeButton = document.querySelector("#setting-mode input");
 
-largerTextButton.addEventListener("click", () => {
+let largerCheckBox = document.querySelector("#larger-text input");
+const biggerTextBar = document.getElementById("bigger-text-bar");
+const pushAlarmBar = document.getElementById("push-alarm-bar");
+const darkThemeBar = document.getElementById("dark-theme-bar");
+
+// 글씨 크게 보기
+biggerTextBar.addEventListener("click", () => {
+  enlargedAction();
+});
+
+largerCheckBox.addEventListener("click", () => {
+  enlargedAction();
+});
+
+function enlargedAction(){
   if (wrap.classList.contains("enlarged")) {
     wrap.classList.remove("enlarged");
+    largerCheckBox.checked = false;
     } else {
-        wrap.classList.add("enlarged");
+      wrap.classList.add("enlarged");
+      largerCheckBox.checked = true;
     }
-  });
+  }
 
+
+  // 다크 테마 적용
 changeModeButton.addEventListener("click", () => {
   if (document.querySelector('body').classList.contains('dark-mode')) {
     document.body.classList.remove("dark-mode");
