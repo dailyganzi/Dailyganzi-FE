bc9a7c62208be89a25e923e575eeb95b67573511
Kim-Sarang-S2 <sarang4264@naver.com>
2023-07-29 21:38:00 +0900

    추가: dark.js  함수 변경, 바 클릭 시 버튼 활성화 - 2

--- scripts/dark.js
+++ scripts/dark.js
@@ -1,38 +1,47 @@
 const wrap = document.querySelector(".wrap");
 
 const largerTextButton = document.querySelector("#larger-text input");
+const pushAlarmButton = document.querySelector("#push-alarm-bar input")
 const changeModeButton = document.querySelector("#setting-mode input");
 
-let largerCheckBox = document.querySelector("#larger-text input");
 const biggerTextBar = document.getElementById("bigger-text-bar");
 const pushAlarmBar = document.getElementById("push-alarm-bar");
 const darkThemeBar = document.getElementById("dark-theme-bar");
 
 // 글씨 크게 보기
+function enlargedAction(){
+  if (wrap.classList.contains("enlarged")) {
+    wrap.classList.remove("enlarged");
+    largerTextButton.checked = false;
+    } else {
+      wrap.classList.add("enlarged");
+      largerTextButton.checked = true;
+    }
+  }
+
 biggerTextBar.addEventListener("click", () => {
   enlargedAction();
 });
 
-largerCheckBox.addEventListener("click", () => {
+largerTextButton.addEventListener("click", () => {
   enlargedAction();
 });
 
-function enlargedAction(){
-  if (wrap.classList.contains("enlarged")) {
-    wrap.classList.remove("enlarged");
-    largerCheckBox.checked = false;
+// 다크 테마 적용
+function modeChangedAction() {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove("dark-mode");
+    changeModeButton.checked = false;
     } else {
-      wrap.classList.add("enlarged");
-      largerCheckBox.checked = true;
+      document.body.classList.add("dark-mode");
+      changeModeButton.checked = true;
     }
   }
 
-
-  // 다크 테마 적용
 changeModeButton.addEventListener("click", () => {
-  if (document.querySelector('body').classList.contains('dark-mode')) {
-    document.body.classList.remove("dark-mode");
-    } else {
-        document.body.classList.add("dark-mode");
-    }
+  modeChangedAction();
+  });
+  
+darkThemeBar.addEventListener("click", () => {
+  modeChangedAction();
   });
