f495d0e5b9306250ec03797420b1ed954b19fb98
Kim-Sarang-S2 <sarang4264@naver.com>
2023-08-02 18:52:59 +0900

    추가: footer.html 병합

---
+++ scripts/active.js
@@ -0,0 +1,15 @@
+const items = document.querySelectorAll("footer .bottom-bar");
+console.log(items);
+items.forEach((item)=>{
+  item.addEventListener('click',()=>{
+    items.forEach((e)=>{
+      // 하나만 선택되도록 기존의 효과 지워주기
+      e.classList.remove('active');
+    })
+      // 선택한 메뉴만 효과 추가해주기
+      item.classList.add('active');
+    })
+})
+
+const feed = document.getElementById("feed");
+console.log(feed);
