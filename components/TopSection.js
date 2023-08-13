a4ec5fb4b5fd412d0e36111e43a9216718493829
reinexxism <lucy0805@naver.com>
2023-08-13 17:46:37 +0900

    변경: component별 .scss file 생성

--- components/TopSection.js
+++ components/TopSection.js
@@ -1,34 +1,36 @@
 // TopSection component
-import Component from "./Component.js";
-import BackButton from "./BackButton.js";
+import Component from './Component.js';
+import BackButton from './BackButton.js';
 
 export default class TopSection extends Component {
-  constructor({ titleText = "타이틀" }) {
-    super({ tagName: "section", props: { titleText } });
+  constructor({ titleText = '타이틀' }) {
+    super({ tagName: 'section', props: { titleText } });
   }
 
   render() {
     this.backButton = new BackButton().el;
-    this.title = document.createElement("h3");
+    this.title = document.createElement('h3');
 
     this.el.appendChild(this.backButton);
     this.el.appendChild(this.title);
 
     this.title.textContent = this.props.titleText;
 
-    this.el.style.position = "relative";
-    this.el.style.width = "100%";
-    this.el.style.height = "50px";
-    this.el.style.marginBottom = "50px";
-    this.el.style.display = "flex";
-    this.el.style.alignItems = "center";
-    this.el.style.justifyContent = "center";
+    this.el.classList.add('top-section');
 
-    this.backButton.style.position = "absolute";
-    this.backButton.style.left = "10px";
+    // this.el.style.position = "relative";
+    // this.el.style.width = "100%";
+    // this.el.style.height = "50px";
+    // this.el.style.marginBottom = "50px";
+    // this.el.style.display = "flex";
+    // this.el.style.alignItems = "center";
+    // this.el.style.justifyContent = "center";
 
-    this.title.style.textAlign = "center";
-    this.title.style.fontSize = "20px";
-    this.title.style.fontWeight = "bold";
+    // this.backButton.style.position = "absolute";
+    // this.backButton.style.left = "10px";
+
+    // this.title.style.textAlign = "center";
+    // this.title.style.fontSize = "20px";
+    // this.title.style.fontWeight = "bold";
   }
 }
