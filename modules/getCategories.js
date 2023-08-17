acb320d6f39f6a9253ca9e90139ddfc313be2014
여다희 <yeodahui.dev@gmail.com>
2023-08-17 16:23:25 +0900

    수정: 이미지 상대경로로 변경

--- modules/getCategories.js
+++ modules/getCategories.js
@@ -1,15 +1,15 @@
 const $categories = document.querySelector("#categories");
 
 const categoryImageMap = {
-  재난: "/asset/images/police_car_light_3d.png",
-  정치: "/asset/images/classical_building_3d.png",
-  경제: "/asset/images/money_bag_3d.png",
-  사회: "/asset/images/busts_in_silhouette_3d.png",
-  IT과학: "/asset/images/dna_3d.png",
-  세계: "/asset/images/globe_with_meridians_3d.png",
-  오피니언: "/asset/images/eyes_3d.png",
-  스포츠: "/asset/images/person_swimming_3d_default.png",
-  생활문화: "/asset/images/shopping_cart_3d.png",
+  재난: "../asset/images/police_car_light_3d.png",
+  정치: "../asset/images/classical_building_3d.png",
+  경제: "../asset/images/money_bag_3d.png",
+  사회: "../asset/images/busts_in_silhouette_3d.png",
+  IT과학: "../asset/images/dna_3d.png",
+  세계: "../asset/images/globe_with_meridians_3d.png",
+  오피니언: "../asset/images/eyes_3d.png",
+  스포츠: "../asset/images/person_swimming_3d_default.png",
+  생활문화: "../asset/images/shopping_cart_3d.png",
 };
 
 const appendListItem = (text, id) => {
